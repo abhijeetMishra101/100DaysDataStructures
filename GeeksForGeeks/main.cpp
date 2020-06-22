0fa03d3e492c328a1cbca42d1ccdd3a1c15d8fa9
abhijeetpharmeasy <abhijeet@pharmeasy.in>
2020-06-22 11:55:12 +0530

    Initial Commit

---
+++ GeeksForGeeks/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GeeksForGeeks
+//
+//  Created by Abhijeet Mishra on 22/06/20.
+//  Copyright © 2020 Abhijeet Mishra. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
